699d99e0ba8e96b26c27bb8c63de24aa887cd25d
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-08 20:09:53 +0900

    add 백준 18108 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/example/Main_18108.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main_18108 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int inputNumber = sc.nextInt();
+
+        /***
+         * 불기 연도를 서기 연도로 변환한 결과를 출력한다.
+         * 불기년도 = 현년 + 544
+         * 따라서 현년 = 불기년도 - 544 (-1 해당년도가 포함이기때문에)
+         * */
+
+        int result = inputNumber - 543;
+
+        System.out.println(result);
+
+    }
+}
