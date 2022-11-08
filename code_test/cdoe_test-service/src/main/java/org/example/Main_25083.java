f0785b8af18551810be51ad8b9dfb958f0a5279e
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-08 22:33:31 +0900

    add 백준 25083 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/example/Main_25083.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main_25083 {
+    public static void main(String[] args) {
+        /**
+         *          ,r'"7
+         * r`-_   ,'  ,/
+         *  \. ". L_r'
+         *    `~\/
+         *       |
+         *       |
+         */
+
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+
+    }
+}
