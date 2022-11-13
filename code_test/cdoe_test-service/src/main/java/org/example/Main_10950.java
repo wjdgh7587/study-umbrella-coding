bf31b0ee75fe860e8b6f934aa08ea444618bbe5b
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-13 21:12:44 +0900

    add 백준 10950 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/example/Main_10950.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String testCaseInputNumber = sc.nextLine();
+
+        List<String> resultArray = new ArrayList<>();
+
+        int figureLoop = Integer.parseInt(testCaseInputNumber);
+        while( figureLoop > 0){
+
+            String testCase = sc.nextLine();
+
+            String[] diceArray = testCase.split(" ");
+            int result = Integer.parseInt(diceArray[0]) + Integer.parseInt(diceArray[1]);
+            resultArray.add(String.valueOf(result));
+
+            figureLoop--;
+        }
+
+        // Result
+        for(int i=0; i<resultArray.size(); i++){
+            System.out.println(resultArray.get(i).toString());
+        }
+    }
+}
