c5f6fc40c3ec34d452b42b8d49e3e6073690c9be
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-13 20:19:08 +0900

    add 백준 2739 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/example/Main_2739.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main_2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int guguMainNumber = sc.nextInt();
+
+        for(int i=1; i <10; i++){
+            int guguResult = i*guguMainNumber;
+            System.out.println(guguMainNumber+" * "+i+" = "+guguResult);
+        }
+
+    }
+}
