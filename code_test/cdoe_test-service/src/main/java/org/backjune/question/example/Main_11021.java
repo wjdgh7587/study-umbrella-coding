ae7eb2a4aa85222e8da33812231c6d9ef3719af1
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-14 22:18:39 +0900

    add 백준 11021 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/backjune/question/example/Main_11021.java
@@ -0,0 +1,23 @@
+package org.backjune.question.example;
+
+import java.io.*;
+
+public class Main_11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCaseNumber = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<= testCaseNumber; i++){
+            String inputNumber = br.readLine();
+            String[] inputNumberSep = inputNumber.split(" ");
+            int A = Integer.parseInt(inputNumberSep[0]);
+            int B = Integer.parseInt(inputNumberSep[1]);
+            bw.write("Case #"+i+": "+(A+B)+"\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
