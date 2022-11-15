53684c17a15be79d1f6ec7532cecf693d7becd44
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-15 22:35:29 +0900

    add 백준 10952 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/backjune/question/example/Main_10952.java
@@ -0,0 +1,37 @@
+package org.backjune.question.example;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main_10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<String> resultArray = new ArrayList<>();
+
+        while(true){
+            String[] sep = (br.readLine()).split(" ");
+
+            int numberA = Integer.parseInt(sep[0]);
+            int numberB = Integer.parseInt(sep[1]);
+
+            int result = numberA + numberB;
+
+            if(result == 0){
+                break;
+            }
+
+            resultArray.add(String.valueOf(result));
+        }
+
+        for(String s : resultArray){
+            System.out.println(s);
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
