959b686e83585705b00b5d8f7579e39ee0a44e06
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-17 02:47:34 +0900

    add 백준 10871 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/backjune/question/example/Main_10871.java
@@ -0,0 +1,27 @@
+package org.backjune.question.example;
+
+import java.util.Scanner;
+
+public class Main_10871 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] a = (sc.nextLine()).split(" ");
+
+        int countNumber = Integer.parseInt(a[0]);
+        int baseNumber = Integer.parseInt(a[1]);
+
+        String[] arrayNumber = (sc.nextLine()).split(" ");
+
+        // 실제 기준으로 삼은 카운트 넘버와 배열 사이즈 일치 여부 판별
+        if(arrayNumber.length == countNumber){
+           for(String s : arrayNumber){
+               if(Integer.parseInt(s) < baseNumber){
+                   System.out.print(s+" ");
+               }
+           }
+        }else{
+            System.out.println("Out of range");
+        }
+    }
+}
