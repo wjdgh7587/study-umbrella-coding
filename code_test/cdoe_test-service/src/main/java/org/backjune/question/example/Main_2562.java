b0151f6612be7e22f4d7196f6bfeff3895a53a27
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-18 22:14:05 +0900

    add 백준 2562 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/backjune/question/example/Main_2562.java
@@ -0,0 +1,37 @@
+package org.backjune.question.example;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main_2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> numberArray = new ArrayList<>();
+
+        for(int i=0; i<9; i++){
+            Integer parseInt = Integer.parseInt(br.readLine());
+            numberArray.add(parseInt);
+        }
+
+        int maxValue = 1;
+        int count = 0;
+
+        for(int j=0; j<numberArray.size(); j++){
+
+            if(maxValue < numberArray.get(j)){
+                maxValue = numberArray.get(j);
+                count = j+1;
+            }
+
+        }
+
+        bw.write(maxValue+"\n"+count);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
