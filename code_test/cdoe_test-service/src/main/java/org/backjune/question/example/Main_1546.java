e98df8ff67322f7680ffd904ec360c94bbda67df
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-19 19:17:20 +0900

    add 백준 1546 문제

---
+++ code_test/cdoe_test-service/src/main/java/org/backjune/question/example/Main_1546.java
@@ -0,0 +1,41 @@
+package org.backjune.question.example;
+
+import java.util.*;
+
+public class Main_1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int subjectCount = Integer.parseInt(sc.nextLine());
+
+        List<Double> resultArray = new ArrayList<>();
+
+        String[] subjectArray = sc.nextLine().split(" ");
+
+        int maxSubjectScore = Integer.parseInt(Arrays.stream(subjectArray)
+                .max(Comparator.comparing(value -> Integer.parseInt(value)))
+                .orElseThrow(NoSuchElementException::new));
+
+        //System.out.println("최대값 : "+maxSubjectScore);
+
+        for(String a : subjectArray){
+            resultArray.add(calNewScore(Integer.parseInt(a), maxSubjectScore));
+        }
+
+        Double c = 0.0;
+        for(Double a : resultArray){
+            c = c + a;
+        }
+        //System.out.println("새로운 과목들의 총합 : "+c);
+        System.out.println(c/resultArray.size());
+
+    }
+
+    public static Double calNewScore(int subjectScore, int maxScore){
+
+        Double a = Double.valueOf(subjectScore)/Double.valueOf(maxScore);
+        Double result = a*100;
+
+        return result;
+    }
+}
