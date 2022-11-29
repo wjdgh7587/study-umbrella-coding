7fbbfb9e7c0ea4b887698a606360c6f353ffe284
Jeongho Yun <wjdgh7587@gmail.com>
2022-11-29 23:23:17 +0900

    update 백준 1157 첫번째 틀린 풀이 과정

--- code_test/cdoe_test-service/src/main/java/baekjoon/step/phase6/Main_1157.java
+++ code_test/cdoe_test-service/src/main/java/baekjoon/step/phase6/Main_1157.java
@@ -2,6 +2,10 @@ package baekjoon.step.phase6;
 
 import java.util.*;
 
+/**
+ * 최초에 생각했을때, map으로 알파벳 key를 지정하고 list를 통해 입력받은 알파벳들의 갯수를 사이즈로
+ * 지정하여 판단하려고 했다. 하지만, 정작 그렇게 할 경우 문제에 대한 답을 뽑아내는대에는 큰 문제가 있었다.
+ */
 public class Main_1157 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,8 +24,6 @@ public class Main_1157 {
                 result.get(a).add(a);
             }
         }
-        System.out.println(result.size());
-
         Iterator<String> iterator = result.keySet().iterator();
 
         while(iterator.hasNext()){
