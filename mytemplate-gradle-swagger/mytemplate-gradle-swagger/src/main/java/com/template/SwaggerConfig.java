05b4619a9e8ea94be963c056c52eaa9db5011447
Jeongho Yun <yunjh5370@nuriflex.co.kr>
2023-05-05 02:55:10 +0900

    Add template-gradle swagger setting
    아직 개발 및 테스트 진행 중인데 모듈로 별도로 관리할지는 고민중 아니면 그냥 boot 영역에서 관리하도록 할지...

---
+++ mytemplate-gradle-swagger/mytemplate-gradle-swagger/src/main/java/com/template/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.template;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    public Docket SwaggerConfig(){
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
