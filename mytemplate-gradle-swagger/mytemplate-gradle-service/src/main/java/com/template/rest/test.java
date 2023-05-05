f78a0cad8307d691ccedbe441ec6678327fce749
Jeongho Yun <wjdgh7587@gmail.com>
2023-05-05 11:55:24 +0900

    Update 스웨어 open-api 적용

--- mytemplate-gradle-swagger/mytemplate-gradle-service/src/main/java/com/template/rest/test.java
+++ mytemplate-gradle-swagger/mytemplate-gradle-service/src/main/java/com/template/rest/test.java
@@ -1,6 +1,7 @@
 package com.template.rest;
 
 import com.template.entity.Member;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class test {
 
+
     @GetMapping("/test")
+    @Operation(summary = "Testing")
     public String test(){
 
         Member member = Member.builder()
