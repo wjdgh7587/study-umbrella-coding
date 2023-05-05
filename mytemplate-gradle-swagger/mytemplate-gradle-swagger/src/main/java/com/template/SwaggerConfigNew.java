750138d7b0bfa00019b8adef6f97db1187a7bd23
Jeongho Yun <wjdgh7587@gmail.com>
2023-05-05 11:55:24 +0900

    Update 스웨어 open-api 적용

---
+++ mytemplate-gradle-swagger/mytemplate-gradle-swagger/src/main/java/com/template/SwaggerConfigNew.java
@@ -0,0 +1,18 @@
+package com.template;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfigNew {
+
+    @Bean
+    public OpenAPI openAPI(){
+
+
+
+        return null;
+    }
+}
