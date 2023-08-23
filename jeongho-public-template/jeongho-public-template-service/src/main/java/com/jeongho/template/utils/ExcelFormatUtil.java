07283dedcf17002bf92c3ff9598d5de07112538b
Jeongho Y <wjdgh7587@gmail.com>
2023-08-23 21:28:49 +0900

    Commit ok

--- jeongho-public-template/jeongho-public-template-service/src/main/java/com/jeongho/template/utils/ExcelFormatUtil.java
+++ jeongho-public-template/jeongho-public-template-service/src/main/java/com/jeongho/template/utils/ExcelFormatUtil.java
@@ -6,10 +6,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.apache.poi.hssf.usermodel.HSSFDataFormat;
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +23,7 @@ public class ExcelFormatUtil {
 
     /**
      * Map List
+     *
      * @param response
      * @param param
      * @param <T>
@@ -33,98 +36,109 @@ public class ExcelFormatUtil {
         CellStyle numberCellStyle = wb.createCellStyle();
         numberCellStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("#,##0"));
 
-        // File Name;
+        final List<T> headerParamList = param.get("header");
+        final List<T> bodyParamList = param.get("body");
+
+        // File Name setting;
         final String fileName = "test excel file";
 
-        // Header
-        final String[] header = {"test_header1", "test_header2"};
+        // Excel Header setting;
         Row row = sheet.createRow(0);
-
-        for(int i=0; i< header.length; i++){
+        for (int i = 0; i < headerParamList.size(); i++) {
             Cell cell = row.createCell(i);
-            cell.setCellValue(header[i]);
+            cell.setCellValue(headerParamList.get(i).toString());
         }
 
-        // Body
+        // Excel Body setting;
+        for (int i = 0; i < bodyParamList.size(); i++) {
+            row = sheet.createRow(i + 1);
+
+            // TODO : excel parsing 추가
+
+            int cellCount = 0;
+
+            T object = bodyParamList.get(i);
+
+            Cell cell = null;
+
+            cell = row.createCell(cellCount);
+            cell.setCellValue(object.equals(""));
+            cellCount++;
+
+            cell = row.createCell(cellCount);
+            cell.setCellValue(object.equals(""));
+            cellCount++;
+
+            cell = row.createCell(cellCount);
+            cell.setCellValue(object.equals(""));
+            cellCount++;
+
+            cell = row.createCell(cellCount);
+            cell.setCellValue(object.equals(""));
+            cellCount++;
 
 
+
+
+
+        }
+
+//        for (int i = 0; i < userPointList.size(); i++) {
+//            row = sheet.createRow(i + 1);  //헤더 이후로 데이터가 출력되어야하니 +1
+//
+//            UserPoint userPoint = userPointList.get(i);
+//
+//            Cell cell = null;
+//            cell = row.createCell(0);
+//            cell.setCellValue(userPoint.getId());
+//
+//            cell = row.createCell(1);
+//            cell.setCellValue(userPoint.getUserCode());
+//
+//            cell = row.createCell(2);
+//            cell.setCellValue(userPoint.getUserName());
+//
+//            cell = row.createCell(3);
+//            cell.setCellValue(userPoint.getPayCnt());
+//
+//            cell = row.createCell(4);
+//            cell.setCellStyle(numberCellStyle);      //숫자포맷 적용
+//            cell.setCellValue(userPoint.getPaySum());
+//        }
 
     }
 
-    public List<Map<String, Object>> testArray(){
+    public List<Map<String, Object>> testArray() {
 
         List<Map<String, Object>> resultArray = new ArrayList<>();
 
         Map<String, Object> testMap = new HashMap<>();
 
-        for(int i=0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
 
         }
 
         return null;
     }
 
+    public static List<Method> getObject(Class<?> targetClass){
 
-    /***
-     * 임의의 객체 리스트를 엑셀 파일로 변환해 OutputStream 으로 던짐
-     *
-     */
-    public <T> void exportDataToExcel(OutputStream stream
-            , List<T> data
-            , Class<T> classz) throws IOException {
+        List<Method> getterTargetAllList = new ArrayList<>();
 
-        // Create workbook and sheet
-        Workbook workbook = WorkbookFactory.create(true);
-        Sheet sheet = workbook.createSheet();
-
-    }
-
-    public <T> Map<String, List<T>> generateExcelForm(
-            Map<String, Object> headerMap
-            , List<T> bodyList){
-
-        Map<String, List<T>> resultMap = new HashMap<>();
-
-        return resultMap;
-    }
-
-    /**
-     * Render Excel Header
-     * @param sheet
-     * @param classz
-     * @param <T>
-     */
-    private <T> void renderHeader(Sheet sheet, Class<T> classz){
-
-        // Row 시작 행
-        Row row = sheet.createRow(0);
-        // Col 시작 열
-        int colIdx = 0;
-
-        // Set Header Name
-        for(Field field : classz.getDeclaredFields()) {
-            String headerName;
-            // custom excel annotation 여부 판별
-            if (field.isAnnotationPresent(ExcelCustomAnnotation.class)) {
-                headerName = field.getAnnotation(ExcelCustomAnnotation.class).headerName();
-                row.createCell(colIdx, CellType.STRING).setCellValue(
-                        headerName.equals("") ? field.getName() : headerName
-                );
-                colIdx++;
+        ReflectionUtils.doWithMethods(targetClass, method -> {
+            if(method.getName().startsWith("get") && method.getParameterCount() == 0){
+                getterTargetAllList.add(method);
             }
+        });
 
-        }
+        return getterTargetAllList;
+    }
+
+    public static void main(String[] args) {
 
     }
 
-    /**
-     * Render Excel Body
-     * @param <T>
-     */
-    private <T> void renderBody(){
 
-    }
 
-    
 
 }
