e107858c4e751d2833555dacbbfa493408068471
Jeongho Y <wjdgh7587@gmail.com>
2023-08-24 22:04:44 +0900

    Commit ok

--- jeongho-public-template/jeongho-public-template-domain/src/main/java/com/jeongho/template/entity/params/TestExcel2.java
+++ jeongho-public-template/jeongho-public-template-domain/src/main/java/com/jeongho/template/entity/params/TestExcel2.java
@@ -1,10 +1,12 @@
 package com.jeongho.template.entity.params;
 
 import com.jeongho.template.entity.base.ExcelCustomAnnotation;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.apache.poi.ss.usermodel.Row;
+import org.springframework.core.annotation.Order;
 
 @Getter
 @Setter
@@ -15,3 +17,5 @@ public class TestExcel2{
     private String gender;
 
 }
+
+
